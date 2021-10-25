5689f7453547bf9bbbb5df804010500c5ec5c8af
phi diep <longphi11a5@gmail.com>
2021-10-25 11:39:37 +0900

    github-page deploy test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+export const transpileDependencies = [
+  'vuetify'
+];
+export const publicPath = process.env.NODE_ENV === 'production'
+  ? '/menu-recommendation/'
+  : '/';
