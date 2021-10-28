fe415688c6defde474130f3666183ce579160040
ninhpm95 <ninhpm95@gmail.com>
2021-10-28 20:23:26 +0900

    card display

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -40,7 +36,14 @@
     <v-main>
       <!-- <HelloWorld/> -->
       <v-container>
-      <InputForm/>
+        <InputForm/>
+        <section class="products">
+          <Product
+            v-for="product in products"
+            :key="product.color"
+            :product="product"
+          />
+        </section>
       </v-container>
     </v-main>
   </v-app>
@@ -48,17 +51,62 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import InputForm from './components/InputForm';
+import InputForm from "./components/InputForm";
+import Product from "./components/Product.vue";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     // HelloWorld,
-    InputForm
+    InputForm,
+    Product,
   },
 
   data: () => ({
-    //
+    products: [
+      {
+        title: "Pho",
+        color: "green",
+        bgtext: "FOOD",
+        src: require("./assets/pho.jpg"),
+      },
+      {
+        title: "Pho",
+        color: "blue",
+        bgtext: "FOOD",
+        src: require("./assets/pho.jpg"),
+      },
+    ],
   }),
 };
 </script>
+
+<style>
+* {
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montseratt', sans-serif;
+}
+
+v-container {
+  width: 100vh;
+  min-height: 100vh;
+  overflow: hidden;
+
+  background-color: #EEE;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.products {
+  display: flex;
+  max-width: 1280px;
+  padding: 25px;
+  margin: 0 auto;
+}
+</style>
