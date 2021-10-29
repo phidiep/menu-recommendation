6e0ecdabf14180847cbf1b9ece764ccc4e5162ce
phi diep <longphi11a5@gmail.com>
2021-10-29 11:43:02 +0900

    update product v-tag

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,22 +1,37 @@
 <template>
-  <div class="product">
-    <div class="product-inner">
-      <div class="product-text-wrap">
-        <h2 class="bg-text">{{ product.bgtext }}</h2>
-      </div>
-      <div class="product-image-wrap">
-        <img :src="product.src" class="image" />
-      </div>
-      <div class="product-detail">
-        <h2>{{ product.title }}</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad
-          reprehenderit enim nisi perferendis, odit minus tenetur libero? At
-          ipsum totam illo sequi unde cum molestias expedita neque dicta quidem.
-        </p>
-      </div>
+  <v-card class="mx-auto my-12" max-width="500" max-height="150">
+    <div class="d-flex flex-no-wrap justify-space-between">
+      <v-avatar class="ma-3" size="125" tile>
+        <v-img
+          lazy-src="https://picsum.photos/id/11/10/6"
+          v-bind:src="product.src"
+        ></v-img>
+      </v-avatar>
+
+      <v-card-text>
+        <v-row
+          ><v-card-title class="text-h6" v-text="product.title"> 
+
+          </v-card-title>
+          <!-- <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+          <div class="grey--text ms-4">4.5 (413)</div> -->
+        </v-row>
+        <v-row align="center" class="mx-0">
+          <div class="product-detail">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ad
+            reprehenderit enim nisi perferendis.
+          </div>
+        </v-row>
+      </v-card-text>
     </div>
-  </div>
+  </v-card>
 </template>
 
 <script>
@@ -28,18 +43,22 @@ export default {
 
 <style>
 .product {
-  flex: 1 1 33.333%;
+  /* flex: 1 1 33.333%; */
   width: 100%;
   padding: 25px;
 }
 
 .product-inner {
   position: relative;
-  padding: 25px;
-  box-shadow: 0px 0px 16px rgba(0, 0, 0, 25);
+  /* padding: 25px; */
+  /* box-shadow: 0px 0px 16px rgba(0, 0, 0, 25); */
 }
 
 .product-image-wrap .image {
   width: 100%;
+}
+.product-detail {
+  text-overflow: ellipsis;
+  overflow: hidden;
 }
 </style>
